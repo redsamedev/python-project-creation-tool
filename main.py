772db70ea2f80a7dc0111d0f829694467968811d
redsamedev <redsamegdev@gmail.com>
2024-01-13 19:13:45 +0100

    source code (changed!)

--- main.py
+++ main.py
@@ -1,73 +1,46 @@
-import os
 import time
+import os
 
-#Web Development
-def web_dev():
-    #Selecting The Name of the project
-    name = input("Type a Name: ")
-    print('\n' + "Select the directory: ")
-    dir = input(">>> ")
-    print('\n' + f"Creating {name} Project...")
-    time.sleep(1.25)
-    os.system(f'mkdir {dir}\{name}')
-    time.sleep(2.55)
-    with open(f'{dir}{name}\main.py', "w") as file, open('templates.datas\simple_flask_code', "r") as fk_sim_code:
-        code = fk_sim_code.read()
-        file.write(code)
-        
-    print('\n' + f'{name} was created Successfully!' + '\n' + '\n')
-    input("press enter to exit")
-    exit(0)
-#GUI applications Development
-def gui_app():
-    #Selecting The Name of the project
-    name = input("Type a Name: ")
-    print('\n' + "Select the directory: ")
-    dir = input(">>> ")
-    print('\n' + f"Creating {name} Project...")
-    time.sleep(1.25)
-    os.system(f'mkdir {dir}\{name}')
-    time.sleep(2.55)
-    with open(f'{dir}{name}\main.py', "w") as file, open('templates.datas\simple_gui_code', "r") as gui_sim_code:
-        code = gui_sim_code.read()
-        file.write(code)
-        
-    print('\n' + f'{name} was created Successfully!' + '\n' + '\n')
-    input("press enter to exit")
-    exit(0)
-#Console applications Development
-def console_app():
-    name = input("Type a Name: ")
-    print('\n' + "Select the directory: ")
-    dir = input(">>> ")
-    print('\n' + f"Creating {name} Project...")
-    time.sleep(1.25)
-    os.system(f'mkdir {dir}\{name}')
-    time.sleep(2.55)
-    with open(f'{dir}{name}\main.py', "w") as file, open('Templates.datas\simple_console_code', "r") as console_sim_code:
-        code = console_sim_code.read()
-        file.write(code)
-        
-    print('\n' + f'{name} was created Successfully!' + '\n' + '\n')
-    input("press enter to exit")
-    exit(0)
+def GUIAPP():
+    name = input("Name Of the Project: ")
+    directory = input("Directory Location: ")
+    print("\ncreating the project...\n\n")
+    try:
+        os.system(f'mkdir {directory}\{name}')
+        os.system(f'type nul > {directory}\{name}\main.py')
+        with open('templates.datas\simple_gui_code', "r") as gui_code, open(f'{directory}\{name}\main.py', "w") as python_file:
+            code = gui_code.read()
+            python_file.write(code)
+        print('\n\nthe project was succesfuly created!\n\n\n')
+    except FileNotFoundError or FileExistsError:
+        print("\n\nFile not Found or does not exist\n\nError Code:0F0NF\n\n")
+
 
 def main():
-    print('Welcome to python project creation tool' + '\n' + '\n')
-    alltemplates = ['1)web development', '2)GUI application', '3)Console application']
-    while True:
-        print('\n' + '-----------------------' + '\n')
-        for i in alltemplates:
+    print("\n\nWelcome to Pypt Console APP\n\n")
+    time.sleep(1)
+    print('you can see the tutorial with "tuto" command')
+    user_command_input = input("Type Command: ")
+    if user_command_input == "tuto":
+        print("just setting up the tutorial....")
+        time.sleep(1)
+        print("a window will popup don't close it!!")
+        os.startfile("tuto.exe")
+        main()
+    elif user_command_input == "mk-proj" or user_command_input == "mkpj":
+        list_of_templates = ["\n\n1)Console APP\n", "2)GUI APP\n", "3)Web APP\n"]
+        for i in list_of_templates:
             print(i)
-            print('-----------------------' + '\n')
-        slector = input('Select: ')
-        if slector == '1':
-            print('\n' + 'Taking the Informations...' + '\n')
-            web_dev()
-        elif slector == '2':
-            print('\n' + 'Taking the Informations...' + '\n')
-            gui_app()
-        elif slector == '3':
-            print('\n' + 'Taking the Informations...' + '\n')
-            console_app()
+        print("Select One..\n")
+        selection = input("Select: ")
+        if selection == "1":
+            print("CAPP")
+            exit()
+        elif selection == "2":
+            GUIAPP()
+        elif selection == "3":
+            print("WAPP")
+            exit()
+
+
 main()
